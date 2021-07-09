680f1a65fec2340d6f20e258b71c92a4d290e95f
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2021-07-09 18:18:04 +0900

    email to game_id-fixed

---
+++ app/src/main/java/com/example/logintest/SocketHandler.kt
@@ -0,0 +1,23 @@
+package com.example.logintest
+
+import io.socket.client.IO
+import io.socket.client.Socket
+import java.net.URISyntaxException
+
+object SocketHandler {
+    lateinit var mSocket: Socket
+
+    @Synchronized
+    fun setSocket() {
+        try{
+            mSocket = IO.socket("http://143.248.226.140:3000")
+        } catch(e: URISyntaxException) {
+
+        }
+    }
+
+    @Synchronized
+    fun getSocket(): Socket {
+        return mSocket
+    }
+}
